eff96854f8014f9a286472478836ade04c0f659d
Damas <damas@neptune>
2017-06-14 21:08:13 +0100

    added api file

---
+++ Controllers/api.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"net/http"
+)
+
+//GetSong retrieve a song by id
+func GetSong(res http.ResponseWriter, req *http.Request) {
+	//get song
+}
+
+//GetPlaylist retrieve playlist by id
+func GetPlaylist(res http.ResponseWriter, req *http.Request) {
+	//get playlist
+}
+
+//PostSong post a song
+func PostSong(res http.ResponseWriter, req *http.Request) {
+	//save song
+}
