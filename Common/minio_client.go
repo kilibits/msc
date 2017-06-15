9d420a7f6557df2e6931b2ea2f993c7765a496ba
Damas <uint16@users.noreply.github.com>
2017-06-15 20:40:32 +0100

    making use of init

--- Common/minio_client.go
+++ Common/minio_client.go
@@ -9,35 +9,27 @@ import (
 var minioClient *minio.Client
 var bucketName string
 
-//Init initialize bucket details
-func Init() {
+func init() {
 
 	bucketName = "myBucket"
-
 	enableSSL := true
 	var err error
 
 	if minioClient == nil {
-
 		minioClient, err = minio.New("endpoint", "access-token", "access-secret", enableSSL)
-
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 	}
-
-	//minioClient.ListBuckets()
 }
 
 //GetSharedClient get client to access buckets
 func GetSharedClient() *minio.Client {
-	Init()
 	return minioClient
 }
 
 //GetBucketName get bucket name
 func GetBucketName() string {
-	Init()
 	return bucketName
 }
