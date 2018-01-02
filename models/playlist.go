18457e11dd13645e89b7d3d72e0a50e460bbef3a
Damas <uint16@users.noreply.github.com>
2018-01-02 14:16:41 +0000

    rename record, added empty methods for playlist

--- models/playlist.go
+++ models/playlist.go
@@ -2,7 +2,19 @@ package models
 
 //PlayList playlist of songs
 type PlayList struct {
+	ID          string
 	Key         string
 	PlaylistURL string
 	Songs       []Song
+	Creator     string
+}
+
+//AddSongToPlaylist add new song to playlist
+func (playlist *PlayList) AddSongToPlaylist(songID string) {
+
+}
+
+//RemoveFromPlaylist remove song from playlist
+func (playlist *PlayList) RemoveFromPlaylist(songID string) {
+
 }
